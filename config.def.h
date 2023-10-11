35d1a69008f4d840b5e22aa04f8c2e894a7babc8
Brendan Grau <bsg8376@g.rit.edu>
2023-10-11 16:01:35 -0400

    update font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Anonymice Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
